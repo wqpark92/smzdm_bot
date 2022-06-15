7778fd81d0ad0d342b4d81e44741b33fe5f5a952
wqpark92 <82661211+wqpark92@users.noreply.github.com>
2022-06-15 10:47:52 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=l5w3rhLBLchij6a2cJOpXwp7; device_id=21307064331655259668670600e5f3282d0fcd47b97707d5de0697f042; homepage_sug=c; r_sort_type=score; __jsluid_s=a020b6f10862714eee1ff2517e1e6a23; _zdmA.vid=*; sajssdk_2015_cross_new_user=1; footer_floating_layer=0; ad_date=15; ad_json_feed=%7B%7D; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1655259672; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22181652a254a53b-05ea09adc10852-3e604809-2073600-181652a254b555%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22%24device_id%22%3A%22181652a254a53b-05ea09adc10852-3e604809-2073600-181652a254b555%22%7D; sess=AT-yj2V%2BprOfFyitq8tecFTemIjkXQQ5DNp5Nrc3eI9BHZUuX8xq7wsbv%2FPliEqY6P%2FRgC7Lr0Hh%2BHYsrezQubnMZ0FOdA568V8ZOjVdaWZUeL25SxN%2FBPmvKy4; user=user%3A7595553261%7C7595553261; smzdm_id=7595553261; _zdmA.uid=ZDMA.kjIsQdx93.1655259696.2419200; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1655259696; bannerCounter=%5B%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%5D; amvid=8bc36fbe69b063642c14a94df7c98d0b; _zdmA.time=1655259697645.0.https%3A%2F%2Fwww.smzdm.com%2F'
 
 
 """
